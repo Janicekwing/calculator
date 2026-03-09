e767e09ac184c96d46c5a784e3f1ac2ffb70b1b0
Janice <janicekwhe@gmail.com>
2026-03-08 22:58:10 -0400

    build display layout

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 
 const divide = (a,b) => a / b;
+
+let num1;
+let operator;
+let num2;
+
+// Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
+
+function operate(operator, num1, num2){
+    return operator(num1, num2);
+}
+
+
