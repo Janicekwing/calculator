3becca856bcc38473397ee302726a3907bf62256
Janice <janicekwhe@gmail.com>
2026-03-10 17:45:59 -0400

    add functionality to buttons to appear on display

--- script.js
+++ script.js
@@ -16,4 +16,62 @@ function operate(operator, num1, num2){
     return operator(num1, num2);
 }
 
+let buttonContainer = document.getElementById('button-container');
+buttonContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        switch (target.id) {
+            case '1':
+                display.textContent = "1";
+                break;
+            case '2':
+                display.textContent = "2";
+                break;
+            case '3':
+                display.textContent = "3";
+                break;
+            case '4':
+                display.textContent = "4";
+                break;
+            case '5':
+                display.textContent = "5";
+                break;
+            case '6':
+                display.textContent = "6";
+                break;
+            case '7':
+                display.textContent = "7";
+                break;
+            case '8':
+                display.textContent = "8";
+                break;
+            case '9':
+                display.textContent = "9";
+                break;
+            case '0':
+                display.textContent = "0";
+                break;
+            case 'equal':
+
+                display.textContent = "=";
+                break;
+            case 'minus':
+                display.textContent = "-";
+                break;
+            case 'plus':
+                display.textContent = "+";
+                break;
+            case 'times':
+                display.textContent = "x";
+                break;
+            case 'divide':
+                display.textContent = "/";
+                break;
+            case 'clear':
+                display.textContent = "";
+                break;
+        }
+    }
+);
+
+
 
